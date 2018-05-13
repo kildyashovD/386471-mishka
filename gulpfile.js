de3253f0fb72d1209bb84524eef3681858281438
Kildyashov <denis.kildiaschov@yandex.ru>
2018-05-13 14:49:31 +0300

    Доделывает минификацию

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var include = require("posthtml-include");
-var posthtml = require("gulp-posthtml");
 var server = require("browser-sync").create();
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
@@ -22,7 +20,6 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"));
@@ -31,8 +28,8 @@ gulp.task("style", function() {
 gulp.task("images", function() {
   return gulp.src("source/img/**/*.{png,svg,jpg}")
     .pipe(imagemin([
-      imagemin.jpegtran({ progressive: true }),
-      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true}),
+      imagemin.optipng({ optimizationLevel: 3}),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"));
@@ -66,6 +63,11 @@ gulp.task("clean", function() {
   return del("build");
 });
 
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"));
+});
+
 gulp.task("build", function (done) {
-  run("clean", "copy", "style", "images", "sprite", "html", done);
+  run("clean", "copy", "style", "images", "html", done);
 });
