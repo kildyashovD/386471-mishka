4e7e28fb285e0a0945b01da7a0edc40c023da3da
Kildyashov <denis.kildiaschov@yandex.ru>
2018-05-16 00:32:45 +0300

    Доделывает открытие modal-add

--- source/js/main.js
+++ source/js/main.js
@@ -12,3 +12,32 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+var addHare = document.getElementById("hare");
+var addBowl = document.getElementById("bowl");
+var addToys = document.getElementById("toys");
+var sizeModal = document.querySelector(".modal-add");
+
+addHare.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sizeModal.classList.add("modal-add__show");
+});
+
+addBowl.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sizeModal.classList.add("modal-add__show");
+});
+
+addToys.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sizeModal.classList.add("modal-add__show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (sizeModal.classList.contains("modal-add__show")) {
+      sizeModal.classList.remove("modal-add__show");
+    }
+  }
+});
